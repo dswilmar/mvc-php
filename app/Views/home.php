0770c4f9a81208cbf52314a32580827ca6cdb56e
Wilmar dos Santos <dswilmar@gmail.com>
2021-08-22 17:50:17 -0300

    Criacao de um estilo padrao de CSS e aplicando na view home

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Estrutura MVC</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="./assets/css/style.css">
 </head>
 <body>
-
-	<h1>Estrutura MVC</h1>
-	<p>Comece editando o arquivo ./app/Views/home.php</p>
-
+	<div id="container">
+		<h1>Estrutura MVC</h1>
+		<p>Comece editando o arquivo ./app/Views/home.php</p>
+	</div>
 </body>
 </html>
