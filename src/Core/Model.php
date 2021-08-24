5a87c506866f9498a41aeafce38e30571dffecbc
Wilmar dos Santos <dswilmar@gmail.com>
2021-08-24 20:56:13 -0300

    Implementados metodos para execucao de SQL, insercao, atualizacao, e remocao de registros no banco de dados

--- src/Core/Model.php
+++ src/Core/Model.php
@@ -26,4 +26,96 @@ class Model
         }        
         return $result;
     }
+
+    public function get($table)
+    {
+        $result = array();
+        $sql = 'SELECT * FROM ' . $table;
+        $result = $this->query($sql);
+        return $result;
+    }
+
+    public function insert($table, $data)
+    {
+        $columns = '';
+        $values = '';
+
+        end($data);
+        $lastColumn = key($data);
+
+        foreach ($data as $column => $val) {            
+            
+            if ($column !== $lastColumn) {
+                $columns .= $column . ', ';
+                $values .= ':' . $column . ', ';
+            } else {
+                $columns .= $column;
+                $values .= ':' . $column;
+            }
+        }
+        
+        $sql = "INSERT INTO ". $table ." (". $columns .") VALUES (". $values .")";
+        $this->query($sql, $data);
+    }
+
+    public function update($table, $data, $conditions)
+    {
+        $sql = "UPDATE " . $table . " SET ";
+        end($data);
+        $lastColumn = key($data);
+
+        foreach ($data as $column => $val) {            
+            
+            if ($column !== $lastColumn) {
+                $sql .= $column ."= :" . $column . ", ";
+            } else {
+                $sql .= $column ."= :" . $column;
+            }
+        }
+
+        if (count($conditions) > 0) {
+            $sql .= " WHERE ";
+
+            end($conditions);
+            $lastCondition = key($conditions);
+
+            foreach ($conditions as $condition => $val) {
+            
+                if ($condition != $lastCondition) {
+                    $sql .= $condition ."= :" . $condition . " AND ";
+                } else {
+                    $sql .= $condition ."= :" . $condition;
+                }
+    
+            }
+        }
+
+        $params = array_merge($data, $conditions);
+        $this->query($sql, $params);
+    }
+
+    public function delete($table, $conditions)
+    {
+        $sql = "DELETE FROM " . $table;
+
+        if (count($conditions) > 0) {
+
+            $sql .= " WHERE ";
+
+            end($conditions);
+            $lastCondition = key($conditions);
+
+            foreach ($conditions as $condition => $val) {
+            
+                if ($condition != $lastCondition) {
+                    $sql .= $condition ."= :" . $condition . " AND ";
+                } else {
+                    $sql .= $condition ."= :" . $condition;
+                }
+            }
+        }
+        
+        $this->query($sql, $conditions);
+
+    }
 }
